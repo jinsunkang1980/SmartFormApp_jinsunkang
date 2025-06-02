a38810afe250331c55ac97a00fe23d56920f1117
jinsunkang1980 <jinsunkang1980@gmail.com>
2025-06-02 17:00:28 -0400

    Add files via upload

---
+++ ViewController.m
@@ -0,0 +1,11 @@
+#import "ViewController.h"
+@implementation ViewController
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(60, 200, 250, 50)];
+    label.text = @"Welcome to SmartForm!";
+    label.textColor = [UIColor blackColor];
+    [self.view addSubview:label];
+}
+@end
