e2e28bc2777efc4007415b7f542d7ea49ea24830
jinsunkang1980 <jinsunkang1980@gmail.com>
2025-06-02 16:10:24 -0400

    Update main.m

--- main.m
+++ main.m
@@ -1,8 +1,18 @@
+//
+//  main.m
+//  SmartFormApp
+//
+//  Created on Assignment
+//
+
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
 int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
     }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
 }
