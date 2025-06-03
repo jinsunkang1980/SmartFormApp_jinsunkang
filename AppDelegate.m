64d71232eb55aa3c606ee295557c08ab7ba369aa
jinsunkang1980 <jinsunkang1980@gmail.com>
2025-06-03 16:21:28 -0400

    Update AppDelegate.m

--- AppDelegate.m
+++ AppDelegate.m
@@ -1,48 +1,48 @@
-objc//
+//
 //  AppDelegate.m
+//
 
 #import "AppDelegate.h"
 
 @interface AppDelegate ()
-
 @end
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    NSLog(@"App has launched successfully.");
+    NSLog(@"✅ App launched successfully on VMware macOS 14 + Xcode 15.3!");
     return YES;
 }
 
 #pragma mark - UISceneSession lifecycle
 
 - (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options {
-    NSLog(@"Setting up scene session.");
+    NSLog(@"🔧 Setting up scene session on VMware");
     return [[UISceneConfiguration alloc] initWithName:@"Default Configuration" sessionRole:connectingSceneSession.role];
 }
 
 - (void)application:(UIApplication *)application didDiscardSceneSessions:(NSSet<UISceneSession *> *)sceneSessions {
-    NSLog(@"Scene sessions discarded.");
+    NSLog(@"🗑️ Scene sessions discarded");
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
-    NSLog(@"App is active again!");
+    NSLog(@"🟢 App became active!");
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
-    NSLog(@"App will resign active (incoming call or home button).");
+    NSLog(@"🟡 App will resign active");
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
-    NSLog(@"App entered background.");
+    NSLog(@"⚫ App entered background");
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
-    NSLog(@"App will enter foreground.");
+    NSLog(@"🔵 App will enter foreground");
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
-    NSLog(@"App is about to terminate.");
+    NSLog(@"🔴 App is about to terminate");
 }
 
 @end
