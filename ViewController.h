4b8ef70079d5a707283427b5f61947c4cffc0149
jinsunkang1980 <jinsunkang1980@gmail.com>
2025-06-03 14:37:38 -0400

    Update ViewController.h

--- ViewController.h
+++ ViewController.h
@@ -1,3 +1,20 @@
+objc//
+//  ViewController.h
+
 #import <UIKit/UIKit.h>
+
 @interface ViewController : UIViewController
+
+// Required Assignment Components
+@property (nonatomic, strong) UITextField *nameField;
+@property (nonatomic, strong) UITextField *emailField;
+@property (nonatomic, strong) UISwitch *subscribeSwitch;
+@property (nonatomic, strong) UIButton *submitButton;
+@property (nonatomic, strong) UILabel *statusLabel;
+
+// Bonus Components (Optional)
+@property (nonatomic, strong) UIPickerView *languagePicker;
+@property (nonatomic, strong) NSArray<NSString *> *languages;
+@property (nonatomic, strong) UILabel *selectedLanguageLabel;
+
 @end
