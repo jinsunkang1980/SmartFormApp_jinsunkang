144709ca327f7686018559fb40e414e28b3a06ca
jinsunkang1980 <jinsunkang1980@gmail.com>
2025-06-03 14:35:11 -0400

    Update AppDelegate.m

--- AppDelegate.m
+++ AppDelegate.m
@@ -1,10 +1,52 @@
+objc//
+//  AppDelegate.m
+//  Lec-5
+//
+//  Created by Abhi Patel on 2025-05-26.
+//
+
 #import "AppDelegate.h"
-#import "ViewController.h"
+
+@interface AppDelegate ()
+
+@end
+
 @implementation AppDelegate
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    self.window.rootViewController = [[ViewController alloc] init];
-    [self.window makeKeyAndVisible];
+    NSLog(@"App has launched successfully.");
     return YES;
 }
+
+#pragma mark - UISceneSession lifecycle
+
+- (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options {
+    NSLog(@"Setting up scene session.");
+    return [[UISceneConfiguration alloc] initWithName:@"Default Configuration" sessionRole:connectingSceneSession.role];
+}
+
+- (void)application:(UIApplication *)application didDiscardSceneSessions:(NSSet<UISceneSession *> *)sceneSessions {
+    NSLog(@"Scene sessions discarded.");
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    NSLog(@"App is active again!");
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+    NSLog(@"App will resign active (incoming call or home button).");
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    NSLog(@"App entered background.");
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    NSLog(@"App will enter foreground.");
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+    NSLog(@"App is about to terminate.");
+}
+
 @end
